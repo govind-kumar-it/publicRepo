476f3be2a8377cfc5c48ecddfb444b030666e588
MY <MY@MY-PC>
2020-01-18 15:48:58 +0530

    merge test

--- src/test/thread/FindRepite.java
+++ src/test/thread/FindRepite.java
@@ -9,8 +9,11 @@ import java.util.Set;
 public class FindRepite {
 	public static void main(String[] args) {
 		String s="aa dd afa afa ee ww aa dd";
+		int length =5;
 		String [] ar=s.split(" ");
 		List<String> list= Arrays.asList(ar);
+		
+			System.out.println(" Test ");
 		Set<String> set= new HashSet<>(list);
 		for(String w:set)
 			System.out.println(w +" "+ Collections.frequency(list,w));
