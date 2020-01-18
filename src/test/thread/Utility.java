70b7605c67612f83ff0ab62bdeb741a4683eccb7
MY <MY@MY-PC>
2020-01-18 14:36:11 +0530

    added compare method

--- src/test/thread/Utility.java
+++ src/test/thread/Utility.java
@@ -5,4 +5,21 @@ public class Utility {
 	public String substr(String str){
 		return str.substring(2,4);
 	}
+	public int compare(Employee e1,Employee e2){
+		
+		return e1.names.compareTo(e2.names);
+	}
+}
+
+class Employee{
+	String names;
+
+	public String getNames() {
+		return names;
+	}
+
+	public void setNames(String names) {
+		this.names = names;
+	}
+	
 }
