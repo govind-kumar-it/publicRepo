685ba514c1870533c1f017ca17814a736a6c0e4b
MY <MY@MY-PC>
2020-01-18 14:13:42 +0530

    test

---
+++ src/test/thread/Producer.java
@@ -0,0 +1,13 @@
+package test.thread;
+
+public class Producer {
+
+	public static void main(String[] args) {
+		producer();
+	}
+
+	static void producer() {
+		System.out.println("in producer");
+	}
+
+}
