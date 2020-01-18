3097cc2de1efb7c3360a8a32228a6e4c4ac56daa
MY <MY@MY-PC>
2020-01-18 18:30:34 +0530

    create file

---
+++ src/test/thread/CreateFile.java
@@ -0,0 +1,11 @@
+package test.thread;
+
+public class CreateFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("This class is for create file" );
+		
+	}
+
+}
